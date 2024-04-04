d108804f07658c165dfdf33370e42bd63a95e45b
unknown <saibavesh5559@gmail.com>
2024-04-04 10:14:02 +0530

    adding updations

---
+++ precache-manifest.584db99d893ad1efb3629761645d6e38.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd8b1363d153dde240c52813406778f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c62a677693e34547532",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "dfb7aa34e058509f44e1",
+    "url": "/static/css/main.5b810cc8.chunk.css"
+  },
+  {
+    "revision": "6c62a677693e34547532",
+    "url": "/static/js/2.fd98a78f.chunk.js"
+  },
+  {
+    "revision": "cf6acb57bea48eeed438",
+    "url": "/static/js/3.8ae09a66.chunk.js"
+  },
+  {
+    "revision": "dfb7aa34e058509f44e1",
+    "url": "/static/js/main.f60f3e67.chunk.js"
+  },
+  {
+    "revision": "3b9c93373b28d4e13366",
+    "url": "/static/js/runtime~main.fecad51a.js"
+  },
+  {
+    "revision": "5074173a6ad46f02b504498e44c2a999",
+    "url": "/static/media/1.5074173a.png"
+  },
+  {
+    "revision": "48ab0ef70d71e9cd97ed63d47b4b6b0f",
+    "url": "/static/media/2.48ab0ef7.png"
+  },
+  {
+    "revision": "cb8e01a991b5f5ba4be8335dabeee90d",
+    "url": "/static/media/3.cb8e01a9.png"
+  },
+  {
+    "revision": "aee02bdbf5bd9d8e760c8612a0db55df",
+    "url": "/static/media/4.aee02bdb.png"
+  },
+  {
+    "revision": "4b0133e9a338de5456febc25deb71f06",
+    "url": "/static/media/5.4b0133e9.png"
+  },
+  {
+    "revision": "37ceb6d9f7c651db458a7f604479781d",
+    "url": "/static/media/AboutUsBanner.37ceb6d9.jpg"
+  },
+  {
+    "revision": "d3c146f5db9d89b1feadacb1e95637ee",
+    "url": "/static/media/DeliveryService.d3c146f5.png"
+  },
+  {
+    "revision": "d3ebf6f9ae9533f9d6250c5d09a23c86",
+    "url": "/static/media/Paymentsuccessful21.d3ebf6f9.png"
+  },
+  {
+    "revision": "049f947616469813bf50d1959a8b7868",
+    "url": "/static/media/bestPriceIcon.049f9476.png"
+  },
+  {
+    "revision": "daa5a1964ab43dfc3336db83bde9fdd5",
+    "url": "/static/media/cancellingorderimg.daa5a196.jpg"
+  },
+  {
+    "revision": "aacefceba316c06ccafa08b90b7a3556",
+    "url": "/static/media/levis.aacefceb.png"
+  },
+  {
+    "revision": "6fe1c8d826ccec8852698d1e1944279d",
+    "url": "/static/media/loginimg.6fe1c8d8.png"
+  },
+  {
+    "revision": "3a24047300d3274c5257a9de0429663c",
+    "url": "/static/media/logo.3a240473.png"
+  },
+  {
+    "revision": "7e01f0592872f0bde2a64402a4c585cc",
+    "url": "/static/media/placingorderimg.7e01f059.jpg"
+  },
+  {
+    "revision": "0791fa5ce133ef87d8ce429ae3339cac",
+    "url": "/static/media/profile.0791fa5c.jpg"
+  },
+  {
+    "revision": "9827e76d13e27fe1849f41d22c7ab4ee",
+    "url": "/static/media/refundimg.9827e76d.jpg"
+  },
+  {
+    "revision": "56bd02b2d210efd4d4d515b0677dc859",
+    "url": "/static/media/satisfy.56bd02b2.png"
+  }
+]);
